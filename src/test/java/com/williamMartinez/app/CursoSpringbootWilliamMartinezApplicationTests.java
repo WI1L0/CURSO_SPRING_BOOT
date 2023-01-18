d6ed30a8c10621e7b7de7273fee86a97455be939
WI1L0 <111997657+WI1L0@users.noreply.github.com>
2023-01-17 20:54:50 -0500

    Add files via upload

---
+++ src/test/java/com/williamMartinez/app/CursoSpringbootWilliamMartinezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williamMartinez.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringbootWilliamMartinezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
